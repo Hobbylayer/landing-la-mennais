fef505357f51e0dcc0d19778c046ca6abbc56b39
Javier Alfonso Mora gil <fsjorgeluis@gmail.com>
2021-07-30 18:16:50 -0400

    build update

---
+++ docs/js/canvas.js
@@ -0,0 +1,30 @@
+function drawBalloon(x, y) {
+	var canvas = document.getElementById('canvas-balloon');
+	var ctx = canvas.getContext('2d');
+	ctx.save();
+	ctx.clearRect(0, 0, 550, 400);
+
+	var png = new Image();
+	png.src = './assets/Globo.png';
+	ctx.drawImage(png, x, y, 50, 75);
+	ctx.restore();
+	y -= 2;
+
+	var loopTimer = setTimeout('drawBalloon(' + x + ',' + y + ')', 20); // ejecuta la animación en un timpo determinado
+
+	if (y === 20) {
+		return stop();
+	}
+
+	function stop() {
+		if (loopTimer) {
+			clearTimeout(loopTimer);
+		}
+	}
+}
+
+window.onload = function () {
+	setTimeout(() => {
+		drawBalloon(20, 300);
+	}, 3000);
+};
