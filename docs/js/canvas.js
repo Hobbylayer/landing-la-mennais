9b672b70e000dd92b818afdb7e6ee3bcea6e5001
Javier Alfonso Mora gil <fsjorgeluis@gmail.com>
2021-07-30 19:20:22 -0400

    add loop in the globo

--- docs/js/canvas.js
+++ docs/js/canvas.js
@@ -2,7 +2,7 @@ function drawBalloon(x, y) {
 	var canvas = document.getElementById('canvas-balloon');
 	var ctx = canvas.getContext('2d');
 	ctx.save();
-	ctx.clearRect(0, 0, 550, 400);
+	ctx.clearRect(0, 0, 90, 400);
 
 	var png = new Image();
 	png.src = './assets/Globo.png';
@@ -10,10 +10,19 @@ function drawBalloon(x, y) {
 	ctx.restore();
 	y -= 2;
 
-	var loopTimer = setTimeout('drawBalloon(' + x + ',' + y + ')', 20); // ejecuta la animación en un timpo determinado
+	var loopTimer = setTimeout('drawBalloon(' + x + ',' + y + ')', 30); // ejecuta la animación en un timpo determinado
 
 	if (y === 20) {
-		return stop();
+		stop();
+		setTimeout(() => {
+			setTimeout('drawBalloon(' + x + ',' + 20 + ')', 20);
+		}, 3000);
+	}
+
+	if (y === -74) {
+		stop();
+		drawBalloon(20, 300);
+		// return stop();
 	}
 
 	function stop() {
